f9f71344a3584e1219154b17ae62763e061f9edd
DANIEL DIAZ SANCHEZ <dds@it.uc3m.es>
2016-10-06 12:12:57 +0200

    Adding stdlib.h and strings.h to fix compilatio problems according to #1

--- code/demand-accept.c
+++ code/demand-accept.c
@@ -26,6 +26,8 @@
 /*****************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
 #include <sys/socket.h>
 #include <resolv.h>
 #include <signal.h>
